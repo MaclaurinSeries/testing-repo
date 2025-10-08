d4e82db60631ddfbc6879e3b18e60d825bbbfed2
Christian Budhi Sabdana <cbsabdana@gmail.com>
2025-10-08 18:34:42 +0700

    test

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello from Kubernetes!");
 });
 
+app.get("/test", (req, res) => {
+  res.send("test");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
